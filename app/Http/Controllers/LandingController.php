e3db1e8e1d7310f1698eacbce323224c7d5ff0e7
Michael Saiba <michaelsaiba84@gmail.com>
2022-07-14 15:25:51 +0300

    force new

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -234,7 +234,8 @@ class LandingController extends Controller
             'inline', 1,
             'encryption ', 128,
             'user_agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36',
-            'viewport ', '414x736'
+            'viewport ', '414x736',
+            'force', 1
         );
         $html2pdf->convert();
         //display the PDF file
