28c282471c3e41032dde6d3e40b81342179fd6a1
Michael Saiba <michaelsaiba84@gmail.com>
2022-10-18 13:40:55 +0300

    composer changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,9 +21,9 @@
         <!-- {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script> --}} -->
         <script src="{{ mix('js/app.js') }}" defer></script>
         <script src="//www.sejda.com/js/sejda-js-api.min.js"></script>
-        <script async
+        <!-- <script async
             src="https://api.countapi.xyz/hit/biddersportal.com/26c22b66-d3fe-45dd-9c9e-666925da30cd?callback=websiteVisits">
-        </script>
+        </script> -->
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-HVZJ5KQXEK"></script>
         <script>
