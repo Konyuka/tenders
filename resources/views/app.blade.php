4019f83f6a83f73b8663185cf74b6a1dc74f049e
Michael Saiba <michaelsaiba84@gmail.com>
2022-11-24 10:33:14 +0300

    minified css and js again

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,44 +1,48 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Fonts -->
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> -->
 
-        <!-- Scripts -->
-        @routes
-        <!-- {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script> --}} -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        <script src="//www.sejda.com/js/sejda-js-api.min.js"></script>
-        <!-- <script async
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- <link href="/css/app.css" rel="stylesheet"> -->
+
+    <!-- Scripts -->
+    @routes
+    <!-- {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script> --}} -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="//www.sejda.com/js/sejda-js-api.min.js"></script>
+    <!-- <script async
             src="https://api.countapi.xyz/hit/biddersportal.com/26c22b66-d3fe-45dd-9c9e-666925da30cd?callback=websiteVisits">
         </script> -->
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HVZJ5KQXEK"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HVZJ5KQXEK"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-            gtag('config', 'G-HVZJ5KQXEK');
-        </script>
-        {!! \App\Meta::render() !!}
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+        gtag('config', 'G-HVZJ5KQXEK');
+    </script>
+    {!! \App\Meta::render() !!}
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>
