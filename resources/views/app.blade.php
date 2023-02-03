a145c8e7aa467ecba8f8df61c7be674bc7b618cb
Michael Saiba <michaelsaiba84@gmail.com>
2023-02-03 16:21:52 +0300

    text compression in .htacess

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,21 +33,14 @@
 
     <link rel="icon" type="image/png" href="../img/favicon.png" />
 
-    <!-- Fonts -->
-    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> -->
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <!-- <link href="/css/app.css" rel="stylesheet"> -->
 
     <!-- Scripts -->
     @routes
-    <!-- {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script> --}} -->
     <script src="{{ mix('js/app.js') }}" defer></script>
-    <script src="//www.sejda.com/js/sejda-js-api.min.js"></script>
-    <!-- <script async
-            src="https://api.countapi.xyz/hit/biddersportal.com/26c22b66-d3fe-45dd-9c9e-666925da30cd?callback=websiteVisits">
-        </script> -->
+    <!-- <script src="//www.sejda.com/js/sejda-js-api.min.js"></script> -->
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HVZJ5KQXEK"></script>
     <script>
@@ -71,7 +64,7 @@
 
     <!-- Cloudflare Web Analytics -->
     <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "12d0fb8c1fb34a6b9ef7fac09b4a54f9"}'></script><!-- End Cloudflare Web Analytics -->
-    
+
 </body>
 
 </html>
