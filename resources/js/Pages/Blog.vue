55cddf965e5acc7946440def690343ea4dec1187
Michael Saiba <michaelsaiba84@gmail.com>
2023-02-16 19:42:50 +0300

    push

--- resources/js/Pages/Blog.vue
+++ resources/js/Pages/Blog.vue
@@ -4,7 +4,7 @@
             <TopBanner />
             <MainMenu />
 
-            <!-- check -->
+            <!-- ch+eck -->
 
             <section class="text-gray-600 body-font overflow-hidden">
                 <div class="container px-5 py-24 mx-auto">
